f7ab7d480bb0d79eeec95399e2eb851f5af08b9a
Matthew McCall <Matthew.McCall2@va.gov>
2012-11-02 09:42:31 -0400

    Modified the Document Entity to include Identifier, and prep for input.

--- das/das_wrap_nwhin/src/main/java/org/osehra/das/wrapper/nwhin/C32DocumentEntity.java
+++ das/das_wrap_nwhin/src/main/java/org/osehra/das/wrapper/nwhin/C32DocumentEntity.java
@@ -3,13 +3,30 @@ package org.osehra.das.wrapper.nwhin;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
 
-//@Entity
-//@Table(name="C32_DOCUMENT")
+
+@Entity
+@Table(name="C32_DOCUMENT")
 public class C32DocumentEntity {
 
-//	@Column(name="DOCUMENT")
+	@Id
+	@GeneratedValue
+	@Column(name = "ID")
+	private int id;
+	
+	@Column(name="DOCUMENT")
 	private String document;
+	
+
+	public void setId(int id) {
+		this.id = id;
+	}
+		
+	public int getId() {
+		return id;
+	}
 
 	public void setDocument(String document) {
 		this.document = document;
