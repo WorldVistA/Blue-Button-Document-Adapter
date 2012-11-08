a89a7845c2359a541ff667e7b229be0c55932c30
Matthew McCall <Matthew.McCall2@va.gov>
2012-11-08 08:48:11 -0500

    Added some initial XOM logic to begin XML trans component.

--- das/das_wrap_nwhin/src/main/java/org/osehra/das/wrapper/nwhin/C32DocumentLogic.java
+++ das/das_wrap_nwhin/src/main/java/org/osehra/das/wrapper/nwhin/C32DocumentLogic.java
@@ -1,14 +1,46 @@
 package org.osehra.das.wrapper.nwhin;
 
-import java.io.Serializable;
+import java.io.*;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import nu.xom.*;
+import java.util.*;
+import javax.xml.xpath.*;
+
 
 public class C32DocumentLogic implements Serializable{
+	
+	private static final Log LOG = LogFactory.getLog(C32DocumentLogic.class);
 
 	public String filterDocument (String document) {
 	
-		String filteredDocument = document;
-		
-		return filteredDocument;
-	}
+		  try {
 
+				Builder builder = new Builder();
+				Document doc = builder.build(new StringReader(document));
+
+				/* Just Here to Demonstrate xom reviewing the file, will need to integrate XPATH or XSLT */
+				//Element root = doc.getRootElement();
+				
+				/*Print Root Val */
+				System.out.println("Root:" + root);
+
+			  } catch (IOException io) {
+				System.out.println(io.getMessage());
+				LOG.error(io);
+			  } catch (ParsingException parserr) {
+				LOG.error(parserr);
+			  }
+		
+		
+		
+		String filteredDocument = document;		
+		return filteredDocument;
+		
+		}
+		
+
+	
 }
