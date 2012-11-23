2dd3a489dfbc1b2dda20b7e85ef0a3a145a33944
Matthew McCall <Matthew.McCall2@va.gov>
2012-11-23 00:23:37 -0500

    Added some baseline javadoc notations to methods.

--- das/das_wrap_nwhin/src/main/java/org/osehra/das/BeanUtils.java
+++ das/das_wrap_nwhin/src/main/java/org/osehra/das/BeanUtils.java
@@ -1,6 +1,20 @@
 package org.osehra.das;
 
+/**
+ * A toolkit which can be used across classes.  Duplicative some of the integration packages, should be rolled into them in a later release.
+ * 
+ * @author Steve Monson
+ *
+ */
 public class BeanUtils {
+	
+	/**
+	 * A null safe string comparison tool.
+	 * 
+	 * @param item1		String 1 to be compared.
+	 * @param item2		String 2 to be compared.
+	 * @return			Returns true if strings are equal or are both null, false if unequal and both are not null.
+	 */
 	public static boolean equalsNullSafe(Object item1, Object item2) {
 		if (item1==null && item2==null) {
 			return true;
--- das/das_wrap_nwhin/src/main/java/org/osehra/das/repo/bluebutton/RepositoryWS.java
+++ das/das_wrap_nwhin/src/main/java/org/osehra/das/repo/bluebutton/RepositoryWS.java
@@ -12,21 +12,43 @@ import org.osehra.das.C32Document;
 import org.springframework.web.context.ContextLoader;
 import org.springframework.web.context.WebApplicationContext;
 
+/**
+ * Public facing web services package.
+ * 
+ * @author Steve Monson
+ *
+ */
+
 @WebService(targetNamespace="org.osehra.repo.bluebutton", serviceName="blueButtonRepository")
 public class RepositoryWS {
+	
 	Log logger = LogFactory.getLog(this.getClass());
 
+	/**
+	 * Web Service takes two parameters and returns a list of available documents in the repository.
+	 * 
+	 * @param patientId    	Patient identifier of the individual.
+	 * @param patientName  	Last name of the associated patient.
+	 * @return 				An XML response indicating record date and status.
+	 */
 	@WebMethod
 	public List<DocStatus> getStatus(String patientId, String patientName) {
 		return getRepository().getStatus(patientId, patientName);
 	}
-
+	
+	/**
+	 * Web Service takes two parameters and returns documents when available.
+	 * 
+	 * @param docDate		Date of the requested document.
+	 * @param patientId		Patient identifier of the individual.
+	 * @return				An XML response containing the either date and document in BASE64 encoding, or date and error message.
+	 */
 	@WebMethod
 	public C32Document getDocument(Date docDate, String patientId) {
 		logger.debug("date:" + docDate + " id:" + patientId);
 		return getRepository().getDocument(docDate, patientId);
 	}
-	
+
 	protected Repository getRepository() {
 		WebApplicationContext cc = ContextLoader.getCurrentWebApplicationContext();
 		Repository _repoImpl = cc.getBean(Repository.class);
