aa2dced732f90bafdc1ef0535c20cbee26240741
Steve Monson <Steve.Monson@va.gov>
2012-11-27 13:50:07 -0700

    Removed batch files that are machine specific

--- das/undeploy.bat
+++
@@ -1,2 +0,0 @@
-del C:\dataLocal\EAP-6.0.0.GA\jboss-eap-6.0\standalone\deployments\blue_button-1.1.1.1-SNAPSHOT.war.deployed
-del C:\dataLocal\EAP-6.0.0.GA\jboss-eap-6.0\standalone\deployments\blue_button-1.1.1.1-SNAPSHOT.war
