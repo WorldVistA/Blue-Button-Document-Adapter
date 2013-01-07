f9c9f7ef0bb2b9df72e054ad15b6eb7bf80ade34
Steve Monson <Steve.Monson@va.gov>
2013-01-07 13:51:13 -0700

    Changed JPA model & tests
    Change-Id: I065f1b6d562f4a0af9a8ef31fdb7dc86ab17d321

--- das/das_wrap_nwhin/src/main/java/org/osehra/das/repo/bluebutton/C32DocumentDaoImpl.java
+++ das/das_wrap_nwhin/src/main/java/org/osehra/das/repo/bluebutton/C32DocumentDaoImpl.java
@@ -3,14 +3,12 @@ package org.osehra.das.repo.bluebutton;
 import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.PersistenceUnit;
+import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.osehra.das.wrapper.nwhin.C32DocumentEntity;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
@@ -21,61 +19,45 @@ import org.springframework.transaction.annotation.Transactional;
  */
 public class C32DocumentDaoImpl implements C32DocumentDao {
 	protected static final Log logger = LogFactory.getLog(C32DocumentDaoImpl.class);
-	@PersistenceUnit(unitName="c32")
-	private EntityManagerFactory entityManagerFactory;
+	@PersistenceContext(unitName="c32")
+	private EntityManager entityManager;
 	
 	/**
 	 * 
 	 * @return JPA entity manager
 	 */
-	public EntityManagerFactory getEntityManagerFactory() {
-		return entityManagerFactory;
+	public EntityManager getEntityManager() {
+		return entityManager;
 	}
 
 	/**
 	 * 
 	 * @param entityManager JPA entity manager
 	 */
-	public void setEntityManagerFactory(EntityManagerFactory factory) {
-		this.entityManagerFactory = factory;
+	public void setEntityManager(EntityManager manager) {
+		this.entityManager = manager;
 	}
 
 	/**
 	 * Delegates action to a JPA entity manager's <code>persist</code> method. 
 	 */
-	@Transactional(propagation = Propagation.REQUIRED)
+	@Transactional
 	public void insert(C32DocumentEntity document) {
 		if (logger.isDebugEnabled()) {
 			logger.debug("attempting persist for: " + document);
 		}
-		EntityManager manager = getEntityManagerFactory().createEntityManager();
-		try {
-			manager.persist(document);
-		}
-		finally {
-			if (manager!=null) {
-				manager.close();
-			}
-		}
+		getEntityManager().persist(document);
 	}
 
 	/**
 	 * Delegates action to a JPA entity manager's <code>merge</code> method.
 	 */
-	@Transactional(propagation = Propagation.REQUIRED)
+	@Transactional
 	public void update(C32DocumentEntity document) {
 		if (logger.isDebugEnabled()) {
 			logger.debug("updating document with:" + document);
 		}
-		EntityManager manager = getEntityManagerFactory().createEntityManager();
-		try {
-			manager.merge(document);
-		}
-		finally {
-			if (manager!=null) {
-				manager.close();
-			}
-		}
+		getEntityManager().merge(document);
 	}
 	
 	/**
@@ -85,23 +67,15 @@ public class C32DocumentDaoImpl implements C32DocumentDao {
 	 * @param patientId
 	 * @return List of <code>C32DocumentEntity</code>s matching the patientId
 	 */
-    @SuppressWarnings("unchecked")
+	@SuppressWarnings("unchecked")
 	@Transactional
     public List<C32DocumentEntity> getAllDocuments(String patientId) {
     	if (logger.isDebugEnabled()) {
     		logger.debug("getAllDocuments with :" + patientId);
     	}
-    	EntityManager manager = getEntityManagerFactory().createEntityManager();
-    	try {
-	        Query query = manager.createQuery("SELECT d FROM C32DocumentEntity d WHERE d.icn = :icn");
-	        query.setParameter("icn", patientId);
-	        return (List<C32DocumentEntity>) query.getResultList();
-    	}
-    	finally {
-    		if (manager!=null) {
-    			manager.close();
-    		}
-    	}
+        Query query = getEntityManager().createQuery("SELECT d FROM C32DocumentEntity d WHERE d.icn = :icn");
+        query.setParameter("icn", patientId);
+        return (List<C32DocumentEntity>) query.getResultList();
     }
     
 }
--- das/das_wrap_nwhin/src/test/java/org/osehra/das/repo/bluebutton/C32DocumentDaoImplTest.java
+++ das/das_wrap_nwhin/src/test/java/org/osehra/das/repo/bluebutton/C32DocumentDaoImplTest.java
@@ -3,10 +3,8 @@ package org.osehra.das.repo.bluebutton;
 import java.util.ArrayList;
 import java.util.GregorianCalendar;
 import java.util.List;
-import java.util.Map;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.FlushModeType;
 import javax.persistence.LockModeType;
@@ -18,12 +16,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.osehra.das.wrapper.nwhin.C32DocumentEntity;
 
-public class C32DocumentDaoImplTest extends AbstractDateAwareTests implements EntityManager, EntityManagerFactory {
+public class C32DocumentDaoImplTest extends AbstractDateAwareTests implements EntityManager {
 	C32DocumentDaoImpl dao;
 	@SuppressWarnings("rawtypes")
 	List persistList;
-	List<java.util.Date> closeList;
-	boolean factoryError = false;
 	boolean managerError = false;
 	TestQuery lastQuery;
 
@@ -31,10 +27,8 @@ public class C32DocumentDaoImplTest extends AbstractDateAwareTests implements En
 	@Before
 	public void setup() {
 		dao = new C32DocumentDaoImpl();
-		dao.setEntityManagerFactory(this);
+		dao.setEntityManager(this);
 		persistList = new ArrayList();
-		closeList = new ArrayList<java.util.Date>();
-		factoryError = false;
 		managerError = false;
 		lastQuery = null;
 	}
@@ -43,7 +37,6 @@ public class C32DocumentDaoImplTest extends AbstractDateAwareTests implements En
 	public void insert() {
 		java.util.Date now = new java.util.Date();
 		dao.insert(getNewC32Doc(now));
-		Assert.assertEquals(1, closeList.size());
 		Assert.assertEquals(1, persistList.size());
 		C32DocumentEntity aDoc = (C32DocumentEntity)persistList.get(0);
 		GregorianCalendar cal = new GregorianCalendar();
@@ -61,18 +54,6 @@ public class C32DocumentDaoImplTest extends AbstractDateAwareTests implements En
 	}
 	
 	@Test
-	public void insert_factoryError() {
-		java.util.Date now = new java.util.Date();
-		factoryError = true;
-		try {
-			dao.insert(getNewC32Doc(now));
-			Assert.fail("no error thrown");
-		}
-		catch (RuntimeException ex) {}
-		Assert.assertEquals(0, closeList.size());
-	}
-	
-	@Test
 	public void insert_managerError() {
 		java.util.Date now = new java.util.Date();
 		managerError = true;
@@ -81,7 +62,6 @@ public class C32DocumentDaoImplTest extends AbstractDateAwareTests implements En
 			Assert.fail("error not thrown");
 		}
 		catch (RuntimeException ex) {}
-		Assert.assertEquals(1, closeList.size());
 	}
 	
 	@Test
@@ -92,18 +72,6 @@ public class C32DocumentDaoImplTest extends AbstractDateAwareTests implements En
 	}
 	
 	@Test
-	public void getAllDocuments_factoryError() {
-		factoryError = true;
-		try {
-			dao.getAllDocuments("fred");
-			Assert.fail("error not thrown");
-		}
-		catch (RuntimeException ex) {}
-		Assert.assertEquals(0, closeList.size());
-		Assert.assertNull(lastQuery);
-	}
-
-	@Test
 	public void getAllDocuments_queryError() {
 		managerError = true;
 		try {
@@ -111,7 +79,6 @@ public class C32DocumentDaoImplTest extends AbstractDateAwareTests implements En
 			Assert.fail("error not thrown");
 		}
 		catch (RuntimeException ex) {}
-		Assert.assertEquals(1, closeList.size());
 		assertQueryTried();
 	}
 	
@@ -136,7 +103,6 @@ public class C32DocumentDaoImplTest extends AbstractDateAwareTests implements En
 
 	@Override
 	public void close() {
-		closeList.add(new java.util.Date());
 	}
 
 	@Override
@@ -239,19 +205,4 @@ public class C32DocumentDaoImplTest extends AbstractDateAwareTests implements En
 	public void setFlushMode(FlushModeType arg0) {
 	}
 
-	@Override
-	public EntityManager createEntityManager() {
-		if (factoryError) {
-			throw new RuntimeException("ug");
-		}
-		return this;
-	}
-
-	@Override
-	public EntityManager createEntityManager(@SuppressWarnings("rawtypes") Map arg0) {
-		if (factoryError) {
-			throw new RuntimeException("ug");
-		}
-		return this;
-	}
 }
